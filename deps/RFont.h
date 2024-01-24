5ca7817d0174167a1c732036d46bccef46d777c0
Colleague Riley <50218871+ColleagueRiley@users.noreply.github.com>
2024-01-24 12:45:18 -0500

    Update RFont.h

--- deps/RFont.h
+++ deps/RFont.h
@@ -839,9 +839,7 @@ void RFont_opengl_getError() {
 u32 RFont_create_atlas(u32 atlasWidth, u32 atlasHeight) {
  #if defined(RFONT_DEBUG) && !defined(RFONT_RENDER_LEGACY)
    glEnable(GL_DEBUG_OUTPUT);
-   glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); // For synchronous output
-   glDebugMessageCallback(RFont_debugCallback, 0); // Set the callback function
-   #endif
+			#endif
 
    u32 id = 0;
    glEnable(GL_TEXTURE_2D);
