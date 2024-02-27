9338b7711954ef311c6faf4062cbdf3794aefc90
ColleagueRiley <ColleagueRiley@gmail.com>
2024-02-26 22:31:59 -0500

    fixed (by removing threading)

--- examples/basic.c
+++ examples/basic.c
@@ -8,7 +8,7 @@
 
 void drawLoop(RSGL_window* w); /* I seperate the draw loop only because it's run twice */
 
-RSGL_window* win2;
+static RSGL_window* win2;
 void* loop2(void *);
 
 unsigned char icon[4 * 3 * 3] = {0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF};
@@ -26,7 +26,6 @@ int main() {
     win->fpsCap = 60;
 
     win2 = RSGL_createWindow("subwindow", RSGL_RECT(200, 200, 200, 200), 0);
-    RGFW_createThread(loop2, NULL); /* the function must be run after the window of this thread is made for some reason (using X11) */
 
     /*unsigned short js = RSGL_registerJoystick(win, 0);*/
     unsigned char i, frames = 60;
@@ -47,6 +46,10 @@ int main() {
 
             but not using this method could cause input lag
         */
+
+        RSGL_window_checkEvent(win2);
+        if (win2->event.type == RGFW_quit)
+            running = 0;
 
         while (RSGL_window_checkEvent(win))  {
             if (win->event.type == RSGL_windowAttribsChange) {
@@ -86,7 +89,7 @@ int main() {
         RSGL_setGradient(gradient, 3);
         RSGL_drawTriangle(RSGL_TRIANGLE(RSGL_POINT(20, win->r.h - 20), RSGL_POINT(win->r.w - 20,win->r.h - 20), RSGL_POINT((win->r.w - 40) / 2, 20)), RSGL_RGB(255, 255, 0));
 
-        RSGL_window_clear(win, RSGL_RGB(255, 255, 255));
+        RSGL_window_clear(win, RSGL_RGB(255, 255, 255));    
 
         {                
             RSGL_point3DF points[3] = {RSGL_POINT3DF(20, win->r.h - 20, 0.0f), RSGL_POINT3DF(win->r.w - 20,win->r.h - 20, 0.0f), RSGL_POINT3DF((win->r.w - 40) / 2, 20, 0.0f)};
@@ -97,38 +100,13 @@ int main() {
             RSGL_point3DF center = {points[2].x, (points[2].y + points[0].y) / 2.0f, 0};
             RSGL_basicDraw(RSGL_TRIANGLES_2D, points, tex, center, RSGL_RGB(255, 255, 0), 3);
             
+            if (running == false)
+                break;
+            
             RSGL_window_clear(win2, RSGL_RGB(255, 255, 255));
         }
     }
 
+    RSGL_window_close(win2);
     RSGL_window_close(win);
-}
-
-void* loop2(void* args) {
-    #ifndef __APPLE__
-    RGFW_window* win = win2;
-    win->fpsCap = 60;
-
-    while (running) {
-        /* 
-            not using a while loop here because there is only one event I care about 
-        */
-        RSGL_window_checkEvent(win);
-        /* 
-            I could've also done
-
-            if (RSGL_checkEvents(win) == RSGL_quit)
-        */
-
-        if (win->event.type == RSGL_quit)
-            break;
-    }
-
-    running = 0;
-    RSGL_window_close(win);
-    #else
-    printf("Managing windows on a seperate thread is not supported on MacOS :(\n");
-    #endif
-
-    return NULL;
 }
