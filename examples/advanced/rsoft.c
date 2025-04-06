e179881309983e7ef3f1991337b1c94251d2034a
ColleagueRiley <ColleagueRiley@gmail.com>
2025-04-06 17:56:04 -0400

    update

--- examples/advanced/rsoft.c
+++ examples/advanced/rsoft.c
@@ -9,22 +9,30 @@
 #include "RSGL_rsoft.h"
 #include <stdio.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+
 unsigned char icon[4 * 3 * 3] = {0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF};
 
 int main(void) {
     RGFW_window* win = RGFW_createWindow("RSGL texture example", (RGFW_rect){0, 0, 500, 500}, RGFW_windowCenter | RGFW_windowTransparent);
 	RGFW_window_initBuffer(win);
 
-	RSGL_init(RSGL_AREA(win->r.w, win->r.h), win->buffer);
+	RSGL_init(RSGL_AREA(win->r.w, win->r.h), win->buffer, RSGL_RSoft_renderer());
 
     RGFW_window_setIcon(win, icon, RGFW_AREA(3, 3), 4);
 
     RSGL_point3D rotate = RSGL_POINT3D(0, 0, 0);
 
-    RSGL_texture texture = RSGL_loadImage("logo.png").tex;
+    int w, h, c;
+    u8* logo = stbi_load("logo.png", &w, &h, &c, 4);
+    u32 texture = RSGL_renderCreateTexture(logo, RSGL_AREA(w, h), c);
+    free(logo);
+
     RSGL_texture iconTex = RSGL_renderCreateTexture(icon, RSGL_AREA(3, 3), 4);
 
-    u32 comicSans = RSGL_loadFont("COMICSANS.ttf");
+    RSGL_font* comicSans = RSGL_loadFont("COMICSANS.ttf");
     RSGL_setFont(comicSans);
 
     size_t texTimer = (size_t)time(0);
@@ -39,7 +47,7 @@ int main(void) {
         RSGL_clear(RSGL_RGBA(65, 65, 65, 255));
         rotate.z += 1;
         
-        RSGL_rotate(rotate);
+        RSGL_setRotate(rotate);
         //RSGL_drawTriangle(RSGL_TRIANGLE(RSGL_POINT(0, 500), RSGL_POINT(200, 500), RSGL_POINT(100, 250)), RSGL_RGB(255, 255, 255));
         
         RSGL_setTexture(texture);
@@ -54,6 +62,7 @@ int main(void) {
 		//RGFW_window_checkFPS(win, 60);
 	}
 	
+    RSGL_renderDeleteTexture(texture);
     RSGL_renderDeleteTexture(iconTex);
 	RSGL_free();
 
--- examples/basics/textures.c
+++ examples/basics/textures.c
@@ -7,18 +7,27 @@
 
 #include <stdio.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <time.h>
+
 unsigned char icon[4 * 3 * 3] = {0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF};
 
 int main(void) {
     RGFW_window* win = RGFW_createWindow("RSGL texture example", (RGFW_rect){0, 0, 500, 500},  RGFW_windowCenter);
 		
-	RSGL_init(RSGL_AREA(win->r.w, win->r.h), RGFW_getProcAddress);
+	RSGL_init(RSGL_AREA(win->r.w, win->r.h), RGFW_getProcAddress, RSGL_GL_renderer());
 
     RGFW_window_setIcon(win, icon, RGFW_AREA(3, 3), 4);
 
     RSGL_point3D rotate = RSGL_POINT3D(0, 0, 0);
 
-    u32 texture = RSGL_loadImage("logo.png").tex;
+    int w, h, c;
+    u8* logo = stbi_load("icon.png", &w, &h, &c, 4);
+    u32 texture = RSGL_renderCreateTexture(logo, RSGL_AREA(w, h), c);
+    free(logo);
+
     u32 iconTex = RSGL_renderCreateTexture(icon, RSGL_AREA(3, 3), 4);
 
     size_t texTimer = (size_t)time(0);
@@ -36,11 +45,11 @@ int main(void) {
         RSGL_clear(RSGL_RGB(65, 65, 65));
         rotate.z += 1;
 
-        RSGL_rotate(rotate);
+        RSGL_setRotate(rotate);
         RSGL_drawRect(RSGL_RECT(200, 200, 200, 200), RSGL_RGB(255, 0, 0));
 
         RSGL_setTexture(iconTex);
-        RSGL_rotate(rotate);
+        RSGL_setRotate(rotate);
         RSGL_drawPolygon(RSGL_RECT(20, 20, 50, 50), 8, RSGL_RGB(255, 255, 255));
         
         RSGL_setTexture(texture);
@@ -58,7 +67,7 @@ int main(void) {
             RSGL_setTexture(0);
         }
 
-        RSGL_rotate(rotate);
+        RSGL_setRotate(rotate);
         RSGL_drawTriangle(RSGL_TRIANGLE(RSGL_POINT(0, 500), RSGL_POINT(200, 500), RSGL_POINT(100, 250)), RSGL_RGB(255, 255, 255));
 
 		RSGL_draw();
@@ -68,6 +77,7 @@ int main(void) {
         frameCount++;
     }
 	
+    RSGL_renderDeleteTexture(texture);
     RSGL_renderDeleteTexture(iconTex);
 	RSGL_free();
 
