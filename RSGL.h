100c82f20c4802f4794136e0da952535385f1d9a
Riley Mabb <colleagueRiley@gmail.com>
2026-05-05 10:57:36 -0400

    update copyright year

--- RSGL.h
+++ RSGL.h
@@ -1,6 +1,6 @@
 /*
 *
-* Copyright (c) 2021-25 ColleagueRiley ColleagueRiley@gmail.com
+* Copyright (c) 2021-26 ColleagueRiley ColleagueRiley@gmail.com
 *
 * This software is provided 'as-is', without any express or implied
 * warranty.  In no event will the authors be held liable for any damages
