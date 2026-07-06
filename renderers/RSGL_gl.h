28fd9260d3dc8d0d5c612a9477a2c0af05ee3296
ColleagueRiley <ColleagueRiley@gmail.com>
2026-07-06 02:56:09 -0400

    fix compatablity with opengl versions

--- renderers/RSGL_gl.h
+++ renderers/RSGL_gl.h
@@ -417,16 +417,18 @@ GLuint RSGL_GL_textureFormatToNative(RSGL_textureFormat format) {
 	switch (format) {
 		case RSGL_formatRGB: return GL_RGB;
 		case RSGL_formatRGBA: return GL_RGBA;
-		case RSGL_formatGrayscale: return GL_RED;
-		case RSGL_formatGrayscaleAlpha: return GL_RED;
 		#if !defined(RSGL_GLES2) && !defined(RSGL_GLES3)
 			case RSGL_formatBGR: return GL_BGR;
 			case RSGL_formatBGRA: return GL_BGRA;
 			case RSGL_formatRed: return GL_RED;
+			case RSGL_formatGrayscale: return GL_RED;
+			case RSGL_formatGrayscaleAlpha: return GL_RED;
 		#else
 			case RSGL_formatBGR: RSGL_ASSERT(!"GLES does not support BGR"); break;
 			case RSGL_formatBGRA: RSGL_ASSERT(!"GLES does not support BGRA"); break;
 			case RSGL_formatRed: RSGL_ASSERT(!"GLES does not support GL RED"); break;
+			case RSGL_formatGrayscale: RSGL_ASSERT(!"GLES does not support GL RED"); break;
+			case RSGL_formatGrayscaleAlpha: RSGL_ASSERT(!"GLES does not support GL RED"); break;
 		#endif
 		default: break;
 	}
